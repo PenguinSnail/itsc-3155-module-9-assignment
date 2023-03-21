c448bae40cf49afec42ef40bee48fc41eed9b5fb
Noah Piraino <noahapiraino@gmail.com>
2023-03-21 19:57:21 -0400

    edit movie form, functionality, and tests

--- app.py
+++ app.py
@@ -73,14 +73,26 @@ def get_single_movie(movie_id: int):
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-
-    return render_template('edit_movies_form.html', id=movie_id)
+    movie = movie_repository.get_movie_by_id(movie_id=movie_id)
+    if movie:
+        return render_template('edit_movies_form.html', id=movie_id, title=movie.title, director=movie.director, rating=movie.rating)
+    return redirect('/movies')
 
 
 @app.post('/movies/<int:movie_id>')
 def update_movie(movie_id: int):
-    # TODO: Feature 5
-    # After updating the movie in the database, we redirect back to that single movie page
+    title = request.form.get("movie_title").strip()
+    director = request.form.get("movie_director").strip()
+    rating = request.form.get("movie_rating", type=int)
+
+    # check all fields exist and rating is between 1 and 5
+    if not title or not director or not rating or rating > 5 or rating < 1:
+        movie = movie_repository.get_movie_by_id(movie_id=movie_id)
+        if movie:
+            return render_template('edit_movies_form.html', id=movie_id, title=movie.title, director=movie.director, rating=movie.rating, error=True)
+        return redirect('/movies')
+
+    movie_repository.update_movie(movie_id=movie_id, title=title, director=director, rating=rating)
     return redirect(f'/movies/{movie_id}')
 
 
