9b5c2ef946c9aa8e748aa1a66f8312b141d7e19b
Noah Piraino <noahapiraino@gmail.com>
2023-03-22 19:54:35 -0400

    fix failing pytest

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -4,7 +4,6 @@ from src.repositories.movie_repository import get_movie_repository
 def test_get_all_movies():
      movie_repository = get_movie_repository()
      movie_repository.clear_db()
-     movie_repository.create_movie('test move', 'test director', 5)
      test = len(movie_repository.get_all_movies())
      assert test ==0
 
